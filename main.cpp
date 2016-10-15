e3a07c397a262beefc6fd62cd7c4fbb3c1089566
avillarosa <blacknike88@gmail.com>
2016-10-15 16:50:00 -0700

    Added more classes

--- main.cpp
+++ main.cpp
@@ -1,60 +1,73 @@
 #include <iostream>
-#include <vector>
 #include <string>
 using namespace std;
 
-enum Colors {};
-enum Materials {};
+enum Color { BLACK, BLUE, GREEN, CYAN, RED, PURPLE, YELLOW, WHITE };
+enum Material {PLATIC, WOOD, STEEL };
 
-struct Dimensions {
+struct Dimension {
 	int length;
 	int width;
 };
 
-class Table{
-
-private:
-	Colors col;
-	int legs;
-	Dimensions dim;
-	Materials 
-	
-
-public:
-
-
-
-};
 
 class Book{
 private:
+	bool status;
+	Dimension book;
+	int numOfPages;
+	string title;
 	
 public:
 
+	// Setters
+	void set_status(bool s){
+		status = s;
+	}
+	void set_Dimension(int length, int width){
+		length = book.length;
+		width  = book.width;
+	}	
+	void set_numOfPages(int n){
+		numOfPages = n;
+	}
+	void set_title(string s){
+		title = s;
+	} 
 
-
-
+	// Getters
+	bool get_status(){
+		return status;
+	}
+	int get_surfaceArea(){
+		return book.length * book.width;
+	}
+	int get_numOfPages(){
+		return numOfPages;
+	}
+	string get_title(){
+		return title;
+	}
+	
 };
 
 class TissueBox{
 
 private:
 	int numOfTissues;
-	Dimensions dimen;
+	Dimension tissue;
 	int max;
 	
 
-public: 
-	// Default Constructor
-	TissueBox();
+public:
 
 	// Setters
 	void set_numOfTissues(int n){
 		numOfTissues = n;
 	}
-	void set_Dimensions(int length, int width){
-		dimen.length = length;
-		dimen.width  = width;
+	void set_Dimension(int length, int width){
+		tissue.length = length;
+		tissue.width  = width;
 	}
 	void set_max(int m){
 		max = m;
@@ -64,8 +77,8 @@ public:
 	int get_numOfTissues(){
 		return numOfTissues;
 	}
-	int get_SurfaceArea(){
-		return dimen.length * dimen.width;
+	int get_surfaceArea(){
+		return tissue.length * tissue.width;
 	}
 	float get_tissuePercentage{
 		return ((float(numOfTissues))/max) * 100;
@@ -78,10 +91,43 @@ public:
 class Lamp{
 
 private:
+	Color lColor;
+	bool status;
+	Dimension lamp;
 
 public:
 
+	// Setters
+	void set_lColor(Color c){
+		lColor = c;
+	}
+	void set_status(bool s){
+		status = s;
+	}
+	void set_Dimension(int length, int width){
+		lamp.length = length;
+		lamp.width  = width;
+	}
 
+	// Getters
+	Color get_lColor(){
+		return lColor;
+	}
+	bool get_status(){
+		return status;
+	}
+	int get_surfaceArea(){
+		return lamp.length * lamp.width;
+	}
+	
+
+};
+
+
+class Table{
+private:
+
+public: 
 
 
 };
